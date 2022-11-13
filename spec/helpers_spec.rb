d7d689a541a9069981f21ada0cb8806a461a47c2
swaff-y <kyle@swaff.id.au>
2022-11-13 22:24:22 +1100

    Helpers specs

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -3,25 +3,23 @@
 require_relative '../lib/helpers'
 
 RSpec.describe SpecRefLib::Helpers do
+  let(:helper) { described_class }
   context '.log' do
-    xit 'logs the correct value' do
-      m = menu.new
-      expect { m.log('value') }.to output("value\n").to_stdout
+    it 'logs the correct value' do
+      expect { helper.log('value') }.to output("value\n").to_stdout
     end
   end
 
   context '.leave' do
-    xit 'exits' do
-      m = menu.new
-      expect { m.leave }.to raise_error SystemExit
+    it 'exits' do
+      expect { helper.leave }.to raise_error SystemExit
     end
   end
 
   context '.ret_value' do
-    xit 'gets the correct value' do
+    it 'gets the correct value' do
       allow($stdin).to receive(:gets).and_return('foo')
-      m = menu.new
-      expect(m.ret_value).to eq 'foo'
+      expect(helper.ret_value).to eq 'foo'
     end
   end
 end

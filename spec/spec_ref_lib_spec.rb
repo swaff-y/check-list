06f19b2a2c039fa2b2012e2da93536f38189f8fe
swaff-y <kyle@swaff.id.au>
2022-11-15 23:08:42 +1100

    Updated specs

--- spec/spec_ref_lib_spec.rb
+++ spec/spec_ref_lib_spec.rb
@@ -3,7 +3,48 @@
 require_relative '../lib/spec_ref_lib'
 
 RSpec.describe SpecRefLib::Start do
-  it 'intializes correctly'
-  it 'sets options'
-  it 'handles the selection'
+  let(:start) { described_class }
+
+  context ".new" do
+    before do
+      allow(SpecRefLib::HandleFile).to receive(:new).and_return('new_file_handler')
+      allow_any_instance_of(start).to receive(:set_options).and_return('options')
+      allow_any_instance_of(start).to receive(:handler).and_return('handler')
+    end
+    it 'intializes correctly' do
+      expect { start.new }.not_to raise_error
+    end
+  end
+
+  context ".set_options" do
+    before do
+      allow(SpecRefLib::HandleFile).to receive(:new).and_return('new_file_handler')
+      allow(Optimist).to receive(:options).and_return('options')
+      allow_any_instance_of(start).to receive(:handler).and_return('handler')
+    end
+    it 'set options' do
+      s = start.new
+      expect { s.set_options }.not_to raise_error
+    end
+  end
+
+  context ".handler" do
+    before do
+      allow(SpecRefLib::HandleFile).to receive(:new).and_return('new_file_handler')
+      allow(SpecRefLib::Menu).to receive(:new).and_return('new_menu')
+      allow(SpecRefLib::Method).to receive(:new).and_return('new_method')
+    end
+
+    it 'handles a nil method selection' do
+      allow_any_instance_of(start).to receive(:set_options).and_return({ :method => nil })
+      s = start.new
+      expect(s.handler).to eq 'new_menu'
+    end
+
+    it 'handles a not_nil method selection' do
+      allow_any_instance_of(start).to receive(:set_options).and_return({ :method => 'not_nil' })
+      s = start.new
+      expect(s.handler).to eq 'new_method'
+    end
+  end
 end

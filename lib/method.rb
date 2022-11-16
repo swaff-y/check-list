aba13c097767b9f016762aa7ad09aa58ef9b7376
swaff-y <kyle@swaff.id.au>
2022-11-16 21:58:02 +1100

    method option with specs

--- lib/method.rb
+++ lib/method.rb
@@ -6,15 +6,37 @@ require_relative 'handle_file'
 module SpecRefLib
   # Displays the keyword/method provided
   class Method
-    def initialize(filepath, _method)
+    def initialize(filepath, method)
+      @found = false
       SpecRefLib::Helpers.clear
+      @keyword_array = []
       @json = filepath.fetch_json
-      # validate / find method(method)
-      show_method
+      show_method(method)
     end
 
-    def show_method
-      SpecRefLib::Helpers.log 'Showing method here'
+    def show_method(method)
+      @json['categories'].each do |category|
+        category['categories'].each do |cat|
+          next unless cat['keywords'].include? method
+
+          SpecRefLib::Helpers.log cat['example']
+          SpecRefLib::Helpers.log ''
+          @found = true
+        end
+      end
+      SpecRefLib::Helpers.log 'No keywords found' unless @found
+      handle_input(SpecRefLib::Helpers.ret_value)
+    end
+
+    def handle_input(value)
+      SpecRefLib::Helpers.clear
+
+      if value == 'q'
+        SpecRefLib::Helpers.log 'Good Bye'
+        return SpecRefLib::Helpers.leave
+      end
+
+      SpecRefLib::Start.new
     end
   end
 end

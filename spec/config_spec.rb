5e7bc55368e71e3323a487045de82aaa2339cec4
swaff-y <kyle@swaff.id.au>
2022-11-22 08:57:00 +1100

    Updated specs

---
+++ spec/config_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../lib/config'
+
+RSpec.describe CheckList::Config do
+  let(:config) { described_class }
+  let(:time) { double('the time', year: 2022, month: 11, day: 22, hour: 14, min: 15  )}
+
+  it { expect(config.version).to eq '0.0.0' }
+  it { expect(config.default_url).to eq 'https://raw.githubusercontent.com/swaff-y/check-list/main/lib/checklist.json' }
+  it { expect(config.env).to eq 'CHECK_LIST' }
+
+  it 'gets the current time' do
+    allow(Time).to receive(:now).and_return(time)
+    expect(config.time_now).to eq '2022-11-22 14:15'
+  end
+end

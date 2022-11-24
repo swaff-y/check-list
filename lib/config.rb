7b1b2d41536b6782310b64be1d2a8d691f86e11a
swaff-y <kyle@swaff.id.au>
2022-11-24 23:20:24 +1100

    fixed rubo cop warnings

--- lib/config.rb
+++ lib/config.rb
@@ -17,7 +17,12 @@ module CheckList
 
     def self.time_now
       t = Time.now
-      "#{t.year}-#{t.month.to_s.rjust(2, "0")}-#{t.day.to_s.rjust(2, "0")} #{t.hour.to_s.rjust(2, "0")}:#{t.min.to_s.rjust(2, "0")}"
+      year = t.year
+      month = t.month.to_s.rjust(2, '0')
+      day = t.day.to_s.rjust(2, '0')
+      hr = t.hour.to_s.rjust(2, '0')
+      min = t.min.to_s.rjust(2, '0')
+      "#{year}-#{month}-#{day} #{hr}:#{min}"
     end
   end
 end

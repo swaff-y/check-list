578624ec985faccb85711b05f6f08acddbbe79a4
swaff-y <kyle@swaff.id.au>
2022-11-26 23:57:30 +1100

    added cli display

--- lib/display_results.rb
+++ lib/display_results.rb
@@ -10,8 +10,31 @@ module CheckList
 
     private
 
+    # rubocop:disable Metrics/MethodLength
     def display_results
-      # CheckList::Helpers.log @results
+      CheckList::Helpers.log "#{@results[:name]} Checklist -> #{@results[:ref]}"
+      CheckList::Helpers.log ''
+      @results[:tasks].each_with_index do |task, index|
+        CheckList::Helpers.log "#{CheckList::Helpers.white}  #{index + 1}. #{task[:name]}"
+        @results[:tasks][index][:subTasks].each_with_index do |sub_task, idx|
+          color = CheckList::Helpers.white
+          case sub_task[:status]
+          when 'y'
+            color = CheckList::Helpers.green
+            mark = '(/)'
+          when 'n'
+            color = CheckList::Helpers.red
+            mark = '(x)'
+          when 'na'
+            color = CheckList::Helpers.yellow
+            mark = '(*)'
+          end
+          # rubocop:disable Layout/LineLength
+          CheckList::Helpers.log "#{color}    #{idx + 1}. #{sub_task[:name]}#{CheckList::Helpers.white} #{mark} #{sub_task[:time]}"
+          # rubocop:enable Layout/LineLength
+        end
+      end
     end
+    # rubocop:enable Metrics/MethodLength
   end
 end
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -22,5 +22,37 @@ module CheckList
     def self.system_cmd(cmd)
       `#{cmd}`
     end
+
+    def self.bg_red
+      "\x1b[41m"
+    end
+
+    def self.bg_green
+      "\x1b[42m"
+    end
+
+    def self.bg_yellow
+      "\x1b[43m"
+    end
+
+    def self.bg_white
+      "\x1b[47m"
+    end
+
+    def self.red
+      "\x1b[31m"
+    end
+
+    def self.green
+      "\x1b[32m"
+    end
+
+    def self.yellow
+      "\x1b[33m"
+    end
+
+    def self.white
+      "\x1b[37m"
+    end
   end
 end

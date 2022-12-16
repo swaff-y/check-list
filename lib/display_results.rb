dc02de21dc59002c1ba13fa5735a482dc1f7bd59
swaff-y <kyle@swaff.id.au>
2022-12-16 18:34:34 +1100

    Updated final display to show colors

--- lib/display_results.rb
+++ lib/display_results.rb
@@ -11,27 +11,36 @@ module CheckList
 
     private
 
+    def make_mark(status)
+      case status
+      when 'y'
+        "#{make_color(status)}(/)#{CheckList::Helpers.white}"
+      when 'n'
+        "#{make_color(status)}(x)#{CheckList::Helpers.white}"
+      when 'na'
+        "#{make_color(status)}(*)#{CheckList::Helpers.white}"
+      end
+    end
+
+    def make_color(status)
+      case status
+      when 'y'
+        CheckList::Helpers.green
+      when 'n'
+        CheckList::Helpers.red
+      when 'na'
+        CheckList::Helpers.yellow
+      end
+    end
+
     def display_results
-      puts @results
-      CheckList::Helpers.log "#{@results[:name]} Checklist: #{@results[:ref]}"
+      CheckList::Helpers.clear
+      CheckList::Helpers.log "*#{@results[:name]}* Checklist: #{@results[:ref]}"
       CheckList::Helpers.log ''
       @results[:tasks].each_with_index do |task, index|
-        CheckList::Helpers.log "#{CheckList::Helpers.white}  #{index + 1}. #{task[:name]}"
+        CheckList::Helpers.log "  #{make_color(task[:status])}#{index + 1}.#{CheckList::Helpers.white} #{task[:name]} #{make_mark(task[:status])} _#{task[:time]}_"
         @results[:tasks][index][:subTasks].each_with_index do |sub_task, idx|
-          color = CheckList::Helpers.white
-          case sub_task[:status]
-          when 'y'
-            color = CheckList::Helpers.green
-            mark = '(/)'
-          when 'n'
-            color = CheckList::Helpers.red
-            mark = '(x)'
-          when 'na'
-            color = CheckList::Helpers.yellow
-            mark = '(*)'
-          end
-
-          CheckList::Helpers.log "#{color}    #{idx + 1}. #{sub_task[:name]}#{CheckList::Helpers.white} #{mark} #{sub_task[:time]}"
+          CheckList::Helpers.log "    #{make_color(sub_task[:status])}#{idx + 1}.#{CheckList::Helpers.white} #{sub_task[:name]}#{CheckList::Helpers.white} #{make_mark(sub_task[:status])} _#{sub_task[:time]}_"
         end
       end
       CheckList::Helpers.log ''

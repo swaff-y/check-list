9d46ae7334ec7aac5f13c2bc9781de4acdd91be5
swaff-y <kyle@swaff.id.au>
2022-12-21 20:59:12 +1100

    version 1

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CheckList::Config do
   let(:config) { described_class }
   let(:time) { double('the time', year: 2022, month: 11, day: 22, hour: 14, min: 15  )}
 
-  it { expect(config.version).to eq '0.0.0' }
+  it { expect(config.version).to eq '1.0.0' }
   it { expect(config.default_url).to eq 'https://raw.githubusercontent.com/swaff-y/check-list/main/lib/checklist.json' }
   it { expect(config.env).to eq 'CHECK_LIST' }
   it { expect(config.coverage).to eq 95 }
